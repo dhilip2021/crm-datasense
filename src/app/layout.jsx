f726d5a8191c962e99db624f84875d3080cbbb95
dhilip2021 <dhilip@datasense.in>
2025-05-15 18:16:58 +0530

    icon.css rename

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,9 @@ import 'react-perfect-scrollbar/dist/css/styles.css'
 import '@/app/globals.css'
 
 // Generated Icon CSS Imports
-import '@assets/iconify-icons/generated-icons.css'
+// import '@assets/iconify-icons/generated-icons.css'
+
+import '@assets/iconify-icons/icon.css'
 
 //react-toastify
 import 'react-toastify/dist/ReactToastify.css';
