79b4813f152b1b152dccacba3cb8d98f4158d022
dhilip2021 <dhilip@datasense.in>
2025-07-07 10:31:37 +0530

    comments and organization page created

---
+++ src/app/(dashboard)/comments/page.jsx
@@ -0,0 +1,27 @@
+// MUI Imports
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+
+
+
+
+const Comments = () => {
+
+
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Typography variant='h5'>Comments</Typography>
+        <Divider />
+      </Grid>
+     
+      
+      
+      
+    </Grid>
+  )
+}
+
+export default Comments
---
+++ src/app/(dashboard)/organization/page.jsx
@@ -0,0 +1,27 @@
+// MUI Imports
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+
+
+
+
+const Organization = () => {
+
+
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Typography variant='h5'>Organization</Typography>
+        <Divider />
+      </Grid>
+     
+      
+      
+      
+    </Grid>
+  )
+}
+
+export default Organization
