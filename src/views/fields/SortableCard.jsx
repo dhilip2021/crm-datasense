be73deda9919fba2234e4c9dda7f651cd235d3c0
dhilip2021 <dhilip@datasense.in>
2025-07-26 14:27:04 +0530

    sortable card issue fixed

--- src/views/fields/SortableCard.jsx
+++ src/views/fields/SortableCard.jsx
@@ -2,8 +2,82 @@ import { Grid, Card, CardHeader, CardContent, TextField, Typography, Box, MenuIt
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import OptionMenu from '@core/components/option-menu' // assuming this is your customized one
+import { useState } from 'react'
+
+// function SortableCard({ data, index, handleAction, editFlag }) {
+//   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+//     id: data.slug_label
+//   })
+
+//   const style = {
+//     transform: CSS.Transform.toString(transform),
+//     transition,
+//     cursor: 'move',
+//     zIndex: transform ? 1500 : 'auto',
+//     position: transform ? 'relative' : 'static'
+//   }
+
+//   return (
+//     <Grid item xs={3}>
+//       <Box sx={{ mt: 0, mr: 0, float: 'right', position: 'relative', display: 'flex', alignItems: 'end' }}>
+//         <OptionMenu
+//           iconClassName='text-textPrimary'
+//           options={['Edit', 'Delete']}
+//           onOptionClick={option => handleAction(option, data, index)}
+//         />
+//       </Box>
+//       <Card
+//         sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+//         ref={setNodeRef}
+//         style={style}
+//         {...attributes}
+//         {...listeners}
+//       >
+//         <CardContent sx={{ flexGrow: 2 }}>
+//           <Box>
+//             {data.type === 'text' && (
+//               <TextField
+//                 fullWidth
+//                 label={`${data.label}${data.mandatory === 'yes' ? ' *' : ''}`}
+//                 size='small'
+//                 autoComplete='off'
+//               />
+//             )}
+
+//             {data.type === 'select' && Array.isArray(data.items) && (
+//               <TextField select fullWidth label={data.label} size='small' defaultValue=''>
+//                 {data.items.map((item, id) => (
+//                   <MenuItem key={id} value={item.menu_value}>
+//                     {item.menu_value}
+//                   </MenuItem>
+//                 ))}
+//               </TextField>
+//             )}
+
+//             {data.type === 'text-multiline' && (
+//               <TextField
+//                 fullWidth
+//                 label={`${data.label}${data.mandatory === 'yes' ? ' *' : ''}`}
+//                 size='small'
+//                 autoComplete='off'
+//                 multiline
+//                 rows={2}
+//               />
+//             )}
+
+//             <Typography mt={1} variant='body2' color='text.secondary'>
+//               slug--name : {data?.slug_label}
+//             </Typography>
+//           </Box>
+//         </CardContent>
+//       </Card>
+//     </Grid>
+//   )
+// }
 
 function SortableCard({ data, index, handleAction, editFlag }) {
+  const [selectValue, setSelectValue] = useState('')
+
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
     id: data.slug_label
   })
@@ -25,16 +99,18 @@ function SortableCard({ data, index, handleAction, editFlag }) {
           onOptionClick={option => handleAction(option, data, index)}
         />
       </Box>
+
       <Card
         sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
         ref={setNodeRef}
         style={style}
         {...attributes}
-        {...listeners}
+        // Don't spread `listeners` here
       >
         <CardContent sx={{ flexGrow: 2 }}>
-          <Box>
-            {data.type === 'text' && (
+
+
+           {data.type === 'text' && (
               <TextField
                 fullWidth
                 label={`${data.label}${data.mandatory === 'yes' ? ' *' : ''}`}
@@ -43,17 +119,27 @@ function SortableCard({ data, index, handleAction, editFlag }) {
               />
             )}
 
-            {data.type === 'select' && Array.isArray(data.items) && (
-              <TextField select fullWidth label={data.label} size='small' defaultValue=''>
-                {data.items.map((item, id) => (
-                  <MenuItem key={id} value={item.menu_value}>
-                    {item.menu_value}
-                  </MenuItem>
-                ))}
-              </TextField>
-            )}
 
-            {data.type === 'text-multiline' && (
+          {data.type === 'select' && Array.isArray(data.items) && (
+            <TextField
+              select
+              fullWidth
+              label={data.label}
+              size='small'
+              value={selectValue}
+              onChange={e => setSelectValue(e.target.value)}
+              onPointerDown={e => e.stopPropagation()}
+              onClick={e => e.stopPropagation()}
+            >
+              {data.items.map((item, id) => (
+                <MenuItem key={id} value={item.menu_value}>
+                  {item.menu_value}
+                </MenuItem>
+              ))}
+            </TextField>
+          )}
+
+           {data.type === 'text-multiline' && (
               <TextField
                 fullWidth
                 label={`${data.label}${data.mandatory === 'yes' ? ' *' : ''}`}
@@ -64,14 +150,15 @@ function SortableCard({ data, index, handleAction, editFlag }) {
               />
             )}
 
-            <Typography mt={1} variant='body2' color='text.secondary'>
-              slug-name : {data?.slug_label}
-            </Typography>
-          </Box>
+
+          <Typography mt={1} variant='body2' color='text.secondary'>
+            slug--name : {data?.slug_label}
+          </Typography>
         </CardContent>
       </Card>
     </Grid>
   )
 }
+
 
 export default SortableCard
